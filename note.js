d2334e31c18b1a812bf5a9d09974cea90b244f52
Ben Wells <benwells@chromium.org>
2012-08-22 18:39:55 +1000

    Comments

--- note.js
+++ note.js
@@ -1,14 +1,17 @@
 (function() {
+////////////////////////////////////////////////////////////////////////////////
 // Exported globals
 window.gNotes = ['C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb',
               'G', 'G#/Ab', 'A', 'A#/Bb', 'B'];
 // Middle A frequency could be overridden, so it's exposed.
 window.gMiddleAFrequency = 440;
 
+////////////////////////////////////////////////////////////////////////////////
 // Constants for working out note frequencies
 var gMiddleAIndex = (4 * 12) + 9;
 var gNoteFactor = Math.pow(2, 1 / 12);
 
+////////////////////////////////////////////////////////////////////////////////
 // Exported functions
 window.frequencyForNote = function(octave, note) {
   var noteIndex = (12 * octave) + note;

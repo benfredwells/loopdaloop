84096df790774966ce91705e447bd6b98ae3c148
Ben Wells <benfred@mjaw.com>
2013-07-18 18:25:13 +1000

    Default state being set

--- defaultInstrumentState.js
+++ defaultInstrumentState.js
@@ -6,17 +6,113 @@ var module = {};
 
 module.Default = function() {
   return {
-    oscillatorStates: [
+    oscillators: [
       {
         type: 'sine',
+        octaveOffset: 0,
+        noteOffset: 0,
+        detune: 0
       },
       {
         type: 'square',
+        octaveOffset: 1,
+        noteOffset: 5,
+        detune: -10
       },
       {
         type: 'triangle',
+        octaveOffset: 0,
+        noteOffset: 0,
+        detune: 0
       }
-    ]
+    ],
+    filters: [
+      {
+        enabled: true,
+        type: 'lowpass',
+        q: 0,
+        frequency: {
+          currentContour: 'flat',
+          contours: {
+            flat: {
+              value: 4
+            },
+            oscillating: {
+              centerValue: 4,
+              amplitude: 0.1,
+              frequency: 1
+            },
+            adsr: {
+              initialValue: 4,
+              attackDelay: 0,
+              attackTime: 0.1,
+              attackValue: 8,
+              decayTime: 0.5,
+              attackHold: 0,
+              sustainValue: 4,
+              sustainHold: 0,
+              releaseTime: 0.1,
+              finalValue: 8
+            }
+          }
+        }
+      },
+      {
+        enabled: false,
+        type: 'lowpass',
+        q: 0,
+        frequency: {
+          currentContour: 'flat',
+          contours: {
+            flat: {
+              value: 4
+            },
+            oscillating: {
+              centerValue: 4,
+              amplitude: 0.1,
+              frequency: 1
+            },
+            adsr: {
+              initialValue: 4,
+              attackDelay: 0,
+              attackTime: 0.1,
+              attackValue: 8,
+              attackHold: 0,
+              decayTime: 0.5,
+              sustainValue: 4,
+              sustainHold: 0,
+              releaseTime: 0.1,
+              finalValue: 8
+            }
+          }
+        }
+      }
+    ],
+    envelope: {
+      currentContour: 'adsr',
+      contours: {
+        flat: {
+          value: 1
+        },
+        oscillating: {
+          centerValue: 1,
+          amplitude: 0.1,
+          frequency: 1
+        },
+        adsr: {
+          initialValue: 0,
+          attackDelay: 0,
+          attackTime: 0.1,
+          attackValue: 1,
+          attackHold: 0,
+          decayTime: 0.5,
+          sustainValue: 0.5,
+          sustainHold: 0,
+          releaseTime: 0.3,
+          finalValue: 0
+        }
+      }
+    }
   };
 }
 
--- instrumentState.js
+++ instrumentState.js
@@ -4,14 +4,101 @@ InstrumentState = (function() {
 
 var module = {};
 
+function reportError(error) {
+  console.log(error);
+  console.trace();
+}
+
+function updateSetting(setting, value) {
+  if (value != null)
+    setting.value = value;
+  else
+    reportError('updateSetting error: no value');
+}
+
+module.updateFlatContour = function(flatContour, flatContourState) {
+  if (!flatContourState) {
+    reportError('flatContourState undefined');
+    return;
+  }
+  updateSetting(flatContour.valueSetting, flatContourState.value);
+}
+
+module.updateOscillatingContour = function(oscillatingContour, oscillatingContourState) {
+  if (!oscillatingContourState) {
+    reportError('oscillatingContourState undefined');
+    return;
+  }
+  updateSetting(oscillatingContour.centerValueSetting, oscillatingContourState.centerValue);
+  updateSetting(oscillatingContour.amplitudeSetting, oscillatingContourState.amplitude);
+  updateSetting(oscillatingContour.frequencySetting, oscillatingContourState.frequency);
+}
+
+module.updateADSRContour = function(adsrContour, adsrContourState) {
+  if (!adsrContourState) {
+    reportError('adsrContourState undefined');
+    return;
+  }
+  updateSetting(adsrContour.initialValueSetting, adsrContourState.initialValue);
+  updateSetting(adsrContour.attackDelaySetting, adsrContourState.attackDelay);
+  updateSetting(adsrContour.attackTimeSetting, adsrContourState.attackTime);
+  updateSetting(adsrContour.attackValueSetting, adsrContourState.attackValue);
+  updateSetting(adsrContour.attackHoldSetting, adsrContourState.attackHold);
+  updateSetting(adsrContour.decayTimeSetting, adsrContourState.decayTime);
+  updateSetting(adsrContour.sustainValueSetting, adsrContourState.sustainValue);
+  updateSetting(adsrContour.sustainHoldSetting, adsrContourState.sustainHold);
+  updateSetting(adsrContour.releaseTimeSetting, adsrContourState.releaseTime);
+  updateSetting(adsrContour.finalValueSetting, adsrContourState.finalValue);
+}
+
+module.updateContouredValue = function(contouredValue, contouredValueState) {
+  if (!contouredValueState) {
+    reportError('contouredValueState undefined');
+    return;
+  }
+  updateSetting(contouredValue.currentContourSetting, contouredValueState.currentContour);
+  module.updateFlatContour(contouredValue.contoursByIdentifier[Contour.kFlatContour],
+                           contouredValueState.contours[Contour.kFlatContour]);
+  module.updateOscillatingContour(contouredValue.contoursByIdentifier[Contour.kOscillatingContour],
+                                  contouredValueState.contours[Contour.kOscillatingContour]);
+  module.updateADSRContour(contouredValue.contoursByIdentifier[Contour.kADSRContour],
+                           contouredValueState.contours[Contour.kADSRContour]);
+}
+
+module.updateFilter = function(filter, filterState) {
+  if (!filterState) {
+    reportError('filterState undefined');
+    return;
+  }
+  updateSetting(filter.enabledSetting, filterState.enabled);
+  updateSetting(filter.typeSetting, filterState.type);
+  updateSetting(filter.qSetting, filterState.q);
+  module.updateContouredValue(filter.frequency, filterState.frequency);
+}
+
 module.updateOscillator = function(oscillator, oscillatorState) {
-  oscillator.typeSetting.value = oscillatorState.type;
+  if (!oscillatorState) {
+    reportError('oscillatorState undefined');
+    return;
+  }
+  updateSetting(oscillator.typeSetting, oscillatorState.type);
+  updateSetting(oscillator.octaveOffsetSetting, oscillatorState.octaveOffset);
+  updateSetting(oscillator.noteOffsetSetting, oscillatorState.noteOffset);
+  updateSetting(oscillator.detuneSetting, oscillatorState.detune);
 }
 
 module.updateInstrument = function(instrument, instrumentState) {
-  for (var i = 0; i < instrumentState.oscillatorStates.length; i++) {
-  	module.updateOscillator(instrument.oscillators[i], instrumentState.oscillatorStates[i]);
+  if (!instrumentState) {
+    reportError('instrumentState undefined');
+    return;
   }
+  for (var i = 0; i < instrumentState.oscillators.length; i++) {
+    module.updateOscillator(instrument.oscillators[i], instrumentState.oscillators[i]);
+  }
+  for (var i = 0; i < instrumentState.filters.length; i++) {
+    module.updateFilter(instrument.filters[i], instrumentState.filters[i]);
+  }
+  module.updateContouredValue(instrument.envelope, instrumentState.envelope)
 }
 
 return module;

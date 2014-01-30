e8864090d85bd95ca5a9ff01b6a1c05b5cc00997
Ben Wells <benfred@mjaw.com>
2014-01-30 22:10:06 +1100

    Introduce Setting base class

--- app/instrument/setting.js
+++ app/instrument/setting.js
@@ -3,22 +3,40 @@ Setting = (function() {
 "use strict";
 var module = {};
 
+var Setting = function() {
+	this.value = null;
+}
+
+Setting.prototype.setValue = function(aValue) {
+  value = aValue;
+}
+
 module.Choice = function(choices) {
+  Setting.call(this);
   this.value = choices[0];
   this.choices = choices;
 }
 
+module.Choice.prototype = Object.create(Setting.prototype);
+
 module.Boolean = function() {
+  Setting.call(this);
   this.value = false;
 }
 
+module.Boolean.prototype = Object.create(Setting.prototype);
+
 module.Number = function(min, max) {
+  Setting.call(this);
   this.value = min;
   this.min = min;
   this.max = max;
 }
 
+module.Number.prototype = Object.create(Setting.prototype);
+
 module.copyNumber = function(other) {
+  Setting.call(this);
   var number = new module.Number(other.min, other.max);
   number.value = other.value;
   return number;

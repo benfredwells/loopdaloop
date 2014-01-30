c6c705bb7f972b050f7233443bee785b1928cad7
Ben Wells <benfred@mjaw.com>
2014-01-30 22:56:12 +1100

    Added ModifiableGroup

--- app/instrument/setting.js
+++ app/instrument/setting.js
@@ -17,6 +17,10 @@ Modifiable.prototype.isModified = function() {
 Modifiable.prototype.clearModified = function() {
 }
 
+Modifiable.prototype.setListener = function(listener) {
+  this.listener_ = listener;
+}
+
 Modifiable.prototype.notifyListener = function() {
   if (this.listener_)
     this.listener_.onModifiedChanged();
@@ -83,6 +87,43 @@ module.copyNumber = function(other) {
   var number = new module.Number(other.min, other.max);
   number.value = other.value;
   return number;
+}
+
+module.ModifiableGroup = function() {
+  Modifiable.call(this);
+  this.modifiables_ = [];
+  this.wasModified_ = false;
+}
+
+module.ModifiableGroup.prototype = Object.create(Modifiable.prototype);
+
+module.ModifiableGroup.prototype.isModified = function() {
+  for (var i = 0; i < this.modifiables_.length; i++) {
+    if (this.modifiables_[i].isModified())
+      return true;
+  }
+  return false;
+}
+
+module.ModifiableGroup.prototype.clearModified = function() {
+  this.wasModified_ = false;
+  this.modifiables_.forEach(function (modifiable) {
+    modifable.clearModified();
+  });
+}
+
+// This is called by this group's sub modifiables.
+module.ModifiableGroup.prototype.onModifiedChanged = function() {
+  if (this.wasModified_ != this.isModified())
+    this.notifyListener();
+  this.wasModified_ = this.isModified();
+}
+
+module.ModifiableGroup.prototype.addModifiable = function(modifiable) {
+  this.modifiables_.push(modifiable);
+  modifiable.setListener(this);
+  this.onModifiedChanged();
+  return modifiable;
 }
 
 return module;

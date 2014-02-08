56279216bb8429f345b358b28db68aca0e16d61b
Ben Wells <benfred@mjaw.com>
2014-02-08 21:13:34 +1100

    Added bounds setting fudge factor for Linux Aura bug

--- app/synthWindow.js
+++ app/synthWindow.js
@@ -206,8 +206,8 @@ SynthesizerWrapper.prototype.updateSize = function() {
   var bounds = {};
   bounds.left = null;
   bounds.top = null;
-  bounds.width = kWidth;
-  bounds.height = height;
+  bounds.width = kWidth + 10;
+  bounds.height = height + 30;
   win.setBounds(bounds);
 }
 
